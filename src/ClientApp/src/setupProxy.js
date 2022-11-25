0528bfd55d6bef4ba100906f80d4d691676bcdbe
crzytrane <crzytrane@gmail.com>
2022-11-26 00:19:17 +1300

    got swagger working

--- src/ClientApp/src/setupProxy.js
+++ src/ClientApp/src/setupProxy.js
@@ -7,7 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT
     ? env.ASPNETCORE_URLS.split(';')[0]
     : 'http://localhost:20663';
 
-const context = ['/api/*', '/swagger/*'];
+const context = ['/api/**', '/swagger/**'];
 
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
